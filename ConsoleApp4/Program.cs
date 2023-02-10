88a2f839c928da575ea8e08a2b27d9bf17ece5d6
Meysam03 <alre03001@utb.vaxjo.se>
2023-02-10 10:47:18 +0100

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Transactions;
+
+namespace Inlämmningsppgifter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett tal");
+            int tal1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Skriv in ett till tal");
+            int tal2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Skriv in ett sista tal");
+            int tal3 = int.Parse(Console.ReadLine());
+            int summan = Addera(tal1, tal2, tal3);
+            Console.WriteLine(summan);
+        }
+        static int Addera(int tal1, int tal2, int tal3)
+        {
+            int summan = tal1 + tal2 + tal3;
+            return summan;
+        }
+    }
+}
